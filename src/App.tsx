4c813c1dd99b9fbb2bbfc46312159c1390ad74df
MideczkiAdam <2008adika@gmail.com>
2026-05-12 13:28:05 +0200

    keszen van

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ export type ActivityDay = {
 const App = () => {
 
   const [days, setDays] = useState<ActivityDay[]>([])
+  const [maxAct, setMaxAct] = useState(0)
+  const [color, setColor] = useState("#005500")
 
   useEffect(()=> {
     fetch("data.json")
@@ -21,11 +23,28 @@ const App = () => {
     .then(data => setDays(data))
   },[])
 
+  useEffect(()=> {
+    if(!days) return;
+
+    let max = 0
+    days.forEach(day => {
+      let currentDay = 0;
+
+      day.activities.forEach(act => {
+        currentDay += act.timeInSec})
+
+      if(max < currentDay) max = currentDay
+    })
+
+    setMaxAct(max)
+  },[days])
+
   return (
     <>
+    <input type="color" value={color} onChange={(e) => setColor(e.target.value)} />
       {!days && <div>Loading...</div>}
-      {days && <section className="tileWrapper">
-        {days.map(day => <Tile date={day.date} activities={day.activities}/>)}
+      {days && <section className="tileWrapper" style={{"--my-color": color} as React.CSSProperties}>
+        {days.map(day => <Tile date={day.date} activities={day.activities} maxAct={maxAct}/>)}
         </section>}
     </>
   )
--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -2,12 +2,26 @@ import type { Activity } from "../App"
 
 type TilePropsType = {
     date: string,
-    activities: Activity[]
+    activities: Activity[],
+    maxAct: number
 }
 
-const Tile = ({ date, activities }: TilePropsType) => {
+const Tile = ({ date, activities, maxAct }: TilePropsType) => {
+  let sumActTime = 0;
+
+  activities.forEach(act => sumActTime += act.timeInSec)
+
+
+  const calcColor = () => {
+    if(sumActTime > maxAct * 0.8) return "var(--my-color)"
+    if(sumActTime > maxAct * 0.6) return "oklch(from var(--my-color) l c h / 0.6)"
+    if(sumActTime > maxAct * 0.4) return "oklch(from var(--my-color) l c h / 0.4)"
+    if(sumActTime > maxAct * 0.2) return "oklch(from var(--my-color) l c h / 0.2)"
+    return "gainsboro"
+  }
+
   return (
-    <div className="tile" title={date}></div>
+    <div className="tile" style={{background: calcColor()}} title={date}></div>
   )
 }
 
